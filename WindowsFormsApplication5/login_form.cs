89b7eceafeb0d47c59bdff061092966f241086ec
omariotareq <omar.tareq@live.com>
2019-02-08 13:36:35 +0200

    login form gui

--- WindowsFormsApplication5/login_form.cs
+++ WindowsFormsApplication5/login_form.cs
@@ -7,23 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.Sql;
 
 namespace WindowsFormsApplication5
 {
-    public partial class Form2 : Form
+    public partial class login_form : Form
     {
-        Form1 f1 = new Form1();
-        public Form2()
+        public login_form()
         {
             InitializeComponent();
-            
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            
-            f1.ShowDialog();
         }
     }
 }
