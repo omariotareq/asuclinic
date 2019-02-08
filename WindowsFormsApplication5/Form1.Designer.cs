6b9369142b55238b50fea4f7ea36aef05cb148ee
omariotareq <omar.tareq@live.com>
2019-02-08 15:42:10 +0200

    patient registration

--- WindowsFormsApplication5/Form1.Designer.cs
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -339,6 +339,7 @@
             this.button1.TabIndex = 31;
             this.button1.Text = "Proceed to Examination";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button3
             // 
