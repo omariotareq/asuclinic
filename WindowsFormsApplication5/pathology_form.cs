a1e407ff12723bbccdd61efde4ec0004e183e92c
omariotareq <omar.tareq@live.com>
2019-03-04 21:19:42 +0200

    PATHOLOGY FINISHED

--- WindowsFormsApplication5/pathology_form.cs
+++ WindowsFormsApplication5/pathology_form.cs
@@ -13,6 +13,11 @@ namespace WindowsFormsApplication5
     public partial class pathology_form : Form
     {
         int p_id = 0;
+        DataTable dt_date = new DataTable();
+        BindingSource dateBindingSource = new BindingSource();
+
+        DataTable dt = new DataTable();
+        BindingSource pathBindingSource = new BindingSource();
         public pathology_form(int pa_id, string name, int age)
         {
             InitializeComponent();
@@ -21,7 +26,105 @@ namespace WindowsFormsApplication5
             ageLbl.Text = "" + age;
             p_id = pa_id;
 
+            refreshDateCB();
+            dateCB.Text = "";
         }
+
+        private void refreshDateCB()
+        {
+            dateCB.SelectedIndexChanged -= dateCB_SelectedIndexChanged;
+
+
+            dt_date = DataSet.getPatholgyDates(p_id);
+            dateBindingSource.DataSource = dt_date;
+            dateCB.DataSource = dt_date;
+            dateCB.DisplayMember = "patholgydate";
+
+            dateCB.SelectedIndexChanged += dateCB_SelectedIndexChanged;
+        }
+
+        private void clearBindings()
+        {
+            foreach (Control gb in this.Controls)
+            {
+
+                foreach (Control tb in gb.Controls)
+                {
+                    if (tb is TextBox)
+                    {
+                        tb.DataBindings.Clear();
+                        tb.Text = "";
+                    }
+                    if (tb is ComboBox)
+                    {
+                        tb.DataBindings.Clear();
+                    }
+                    if (tb is CheckBox)
+                    {
+                        ((CheckBox)tb).Checked = false;
+                    }
+                }
+
+            }
+
+
+        }
+
+
+        private void fillData()
+        {
+            try
+            {
+                
+
+                dt = DataSet.getpathologydata(dateCB.Text, p_id);
+                pathBindingSource.DataSource = dt;
+
+                pathDate.DataBindings.Clear();
+
+                this.pathDate.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "patholgydate", true));
+                this.ileumInfReaction.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "ileir", true));
+                this.archDist.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "ilad", true));
+                this.infCells.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "ilic", true));
+                this.neutInf.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "Niil", true));
+                this.erosionTB.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "ilER", true));
+                this.colonInfReaction.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "irco", true));
+                this.cryptDist.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "cradco", true));
+                this.basalLymph.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "balco", true));
+                this.cryptAbc.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "crycco", true));
+                this.laminaEosino.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "lamineosco", true));
+                this.otherIlealFindingsTB.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "otherileal", true));
+                this.colonOtherFindingsTB.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "otherfindings", true));
+                this.finalReportTB.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.pathBindingSource, "finalreport", true));
+
+                if (Convert.ToInt32(dt.Rows[0]["ucil"]) == 1)
+                {
+                    UlcChkbx.Checked = true;
+                }
+                if (Convert.ToInt32(dt.Rows[0]["grail"]) == 1)
+                {
+                    granChkbx.Checked = true;
+                }
+                if (Convert.ToInt32(dt.Rows[0]["lamneutroco"]) == 1)
+                {
+                    laminaNeutroChkbx.Checked = true;
+                }
+                if (Convert.ToInt32(dt.Rows[0]["ulceco"]) == 1)
+                {
+                    colonUlcChkbx.Checked = true;
+                }
+                if (Convert.ToInt32(dt.Rows[0]["granilco"]) == 1)
+                {
+                    colonGranChkbx.Checked = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -30,12 +133,49 @@ namespace WindowsFormsApplication5
 
         private void dateCB_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            clearBindings();
+            fillData();
         }
 
         private void pathology_form_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ulc=0;
+                int gran =0;
+                int lam = 0;
+                int colon_ulc =0;
+                int colon_gran=0;
+                if(UlcChkbx.Checked){
+                    ulc =1 ;
+                }
+                if(granChkbx.Checked){
+                    gran = 1;
+                }
+                if(laminaNeutroChkbx.Checked){
+                    lam = 1;
+                }
+                if(colonUlcChkbx.Checked){
+                    colon_ulc = 1;
+                }
+                if(colonGranChkbx.Checked){
+                    colon_gran =1;
+                }
+
+
+                DataSet.Insertpatholgydata(pathDate.Value.Date, ileumInfReaction.Text, archDist.Text, infCells.Text, neutInf.Text, erosionTB.Text, ulc, gran, otherIlealFindingsTB.Text, colonInfReaction.Text, cryptDist.Text, basalLymph.Text, cryptAbc.Text, laminaEosino.Text, lam, colon_ulc, colon_gran, colonOtherFindingsTB.Text, finalReportTB.Text, p_id);
+                MessageBox.Show("Saved Successfully");
+                refreshDateCB();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
 
--- WindowsFormsApplication5/patients_form.Designer.cs
+++ WindowsFormsApplication5/patients_form.Designer.cs
@@ -41,6 +41,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -161,6 +162,18 @@
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.PowderBlue;
+            this.button5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(729, 482);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Pathology";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // patients_form
             // 
             this.AcceptButton = this.button1;
@@ -168,6 +181,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1028, 616);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -200,5 +214,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
