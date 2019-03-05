66af693042270377646744e111bf6903e82eb7a2
moho1994 <47247185+moho1994@users.noreply.github.com>
2019-03-05 14:46:11 +0200

    ,m

--- WindowsFormsApplication5/surgery_form.Designer.cs
+++ WindowsFormsApplication5/surgery_form.Designer.cs
@@ -1,48 +1,48 @@
 ﻿namespace WindowsFormsApplication5
 {
-	partial class surgery_form
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Surgery_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
 			this.ageLbl = new System.Windows.Forms.Label();
 			this.label20 = new System.Windows.Forms.Label();
 			this.patNameLb = new System.Windows.Forms.Label();
 			this.label18 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label1 = new System.Windows.Forms.Label();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.label5 = new System.Windows.Forms.Label();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.label4 = new System.Windows.Forms.Label();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -53,10 +53,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.ageLbl.AutoSize = true;
 			this.ageLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.ageLbl.Location = new System.Drawing.Point(575, 25);
+			this.ageLbl.Location = new System.Drawing.Point(586, 27);
 			this.ageLbl.Name = "ageLbl";
 			this.ageLbl.Size = new System.Drawing.Size(0, 24);
-			this.ageLbl.TabIndex = 41;
+			this.ageLbl.TabIndex = 46;
 			// 
 			// label20
 			// 
@@ -65,10 +65,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.label20.AutoSize = true;
 			this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label20.Location = new System.Drawing.Point(456, 25);
+			this.label20.Location = new System.Drawing.Point(526, 27);
 			this.label20.Name = "label20";
 			this.label20.Size = new System.Drawing.Size(54, 24);
-			this.label20.TabIndex = 40;
+			this.label20.TabIndex = 45;
 			this.label20.Text = "Age:";
 			// 
 			// patNameLb
@@ -78,10 +78,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.patNameLb.AutoSize = true;
 			this.patNameLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.patNameLb.Location = new System.Drawing.Point(226, 25);
+			this.patNameLb.Location = new System.Drawing.Point(237, 27);
 			this.patNameLb.Name = "patNameLb";
 			this.patNameLb.Size = new System.Drawing.Size(0, 24);
-			this.patNameLb.TabIndex = 39;
+			this.patNameLb.TabIndex = 44;
 			// 
 			// label18
 			// 
@@ -90,11 +90,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.label18.AutoSize = true;
 			this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label18.Location = new System.Drawing.Point(211, 25);
+			this.label18.Location = new System.Drawing.Point(274, 27);
 			this.label18.Name = "label18";
 			this.label18.Size = new System.Drawing.Size(71, 24);
-			this.label18.TabIndex = 38;
+			this.label18.TabIndex = 43;
 			this.label18.Text = "Name:";
+			this.label18.Click += new System.EventHandler(this.label18_Click);
 			// 
 			// label3
 			// 
@@ -103,105 +104,21 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(12, 18);
+			this.label3.Location = new System.Drawing.Point(23, 20);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(116, 31);
-			this.label3.TabIndex = 37;
+			this.label3.TabIndex = 42;
 			this.label3.Text = "Surgery";
 			// 
-			// groupBox1
+			// dateTimePicker1
 			// 
-			this.groupBox1.Controls.Add(this.button1);
-			this.groupBox1.Controls.Add(this.textBox3);
-			this.groupBox1.Controls.Add(this.label5);
-			this.groupBox1.Controls.Add(this.textBox2);
-			this.groupBox1.Controls.Add(this.label4);
-			this.groupBox1.Controls.Add(this.textBox1);
-			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.dateTimePicker1);
-			this.groupBox1.Location = new System.Drawing.Point(12, 72);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(969, 526);
-			this.groupBox1.TabIndex = 42;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-			// 
-			// button1
-			// 
-			this.button1.BackColor = System.Drawing.Color.Orange;
-			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(760, 462);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(148, 45);
-			this.button1.TabIndex = 46;
-			this.button1.Text = "Save";
-			this.button1.UseVisualStyleBackColor = false;
-			// 
-			// textBox3
-			// 
-			this.textBox3.Location = new System.Drawing.Point(246, 351);
-			this.textBox3.Multiline = true;
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(541, 92);
-			this.textBox3.TabIndex = 45;
-			// 
-			// label5
-			// 
-			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(-4, 371);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(210, 24);
-			this.label5.TabIndex = 44;
-			this.label5.Text = "Surgical complication";
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(246, 236);
-			this.textBox2.Multiline = true;
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(541, 92);
-			this.textBox2.TabIndex = 43;
-			// 
-			// label4
-			// 
-			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.label4.AutoSize = true;
-			this.label4.BackColor = System.Drawing.Color.Silver;
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(-4, 246);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(166, 24);
-			this.label4.TabIndex = 42;
-			this.label4.Text = "Operative details";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(246, 132);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(544, 83);
-			this.textBox1.TabIndex = 41;
-			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-			// 
-			// label2
-			// 
-			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(-4, 132);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(182, 24);
-			this.label2.TabIndex = 40;
-			this.label2.Text = "Surgical indication";
+			this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dateTimePicker1.Location = new System.Drawing.Point(266, 32);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(114, 20);
+			this.dateTimePicker1.TabIndex = 47;
 			// 
 			// label1
 			// 
@@ -210,23 +127,105 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(6, 54);
+			this.label1.Location = new System.Drawing.Point(13, 28);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(151, 24);
-			this.label1.TabIndex = 39;
+			this.label1.TabIndex = 48;
 			this.label1.Text = "Date of surgery";
 			// 
-			// dateTimePicker1
+			// groupBox1
 			// 
-			this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
-			this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-			this.dateTimePicker1.Location = new System.Drawing.Point(281, 54);
-			this.dateTimePicker1.Name = "dateTimePicker1";
-			this.dateTimePicker1.Size = new System.Drawing.Size(200, 29);
-			this.dateTimePicker1.TabIndex = 0;
+			this.groupBox1.BackColor = System.Drawing.Color.LightGray;
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.textBox3);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.dateTimePicker1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 85);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(813, 525);
+			this.groupBox1.TabIndex = 49;
+			this.groupBox1.TabStop = false;
 			// 
-			// surgery_form
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(229, 120);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(497, 76);
+			this.textBox1.TabIndex = 49;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(13, 120);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(151, 24);
+			this.label2.TabIndex = 50;
+			this.label2.Text = "Date of surgery";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(6, 246);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(151, 24);
+			this.label4.TabIndex = 51;
+			this.label4.Text = "Date of surgery";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(229, 235);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(497, 76);
+			this.textBox2.TabIndex = 52;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// label5
+			// 
+			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(6, 371);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(151, 24);
+			this.label5.TabIndex = 53;
+			this.label5.Text = "Date of surgery";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(229, 342);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(497, 76);
+			this.textBox3.TabIndex = 54;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Yellow;
+			this.button1.Location = new System.Drawing.Point(518, 437);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(146, 34);
+			this.button1.TabIndex = 55;
+			this.button1.Text = "save";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// Surgery_form
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -239,15 +238,14 @@
 			this.Controls.Add(this.label18);
 			this.Controls.Add(this.label3);
 			this.MaximizeBox = false;
-			this.Name = "surgery_form";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "surgery_form";
+			this.Name = "Surgery_form";
+			this.Text = "Surgery_form";
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
-		}
+        }
 
 		#endregion
 
@@ -256,15 +254,15 @@
 		private System.Windows.Forms.Label patNameLb;
 		private System.Windows.Forms.Label label18;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.DateTimePicker dateTimePicker1;
-		private System.Windows.Forms.TextBox textBox3;
-		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label5;
 	}
 }
--- WindowsFormsApplication5/surgery_form.cs
+++ WindowsFormsApplication5/surgery_form.cs
@@ -10,29 +10,31 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication5
 {
-	public partial class surgery_form : Form
+	public partial class Surgery_form : Form
 	{
-		int p_id = 0;
-		public surgery_form(int pa_id,string name ,int age)
+		public Surgery_form()
 		{
+			int pid = 0;
+			public surgery_results(int pa_id, string name, int age)
+			{
+				InitializeComponent();
+				patNameLb.Text = name;
+				ageLbl.Text = "" + age;
+				pa_id = pa_id;
+
+				
+
+			}
+
 			InitializeComponent();
-			patNameLb.Text = name;
-			ageLbl.Text = "" + age;
-			//hkjhk/
-			p_id = pa_id;
 		}
 
-		private void groupBox1_Enter(object sender, EventArgs e)
+		private void label18_Click(object sender, EventArgs e)
 		{
 
 		}
 
-		private void label1_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void textBox1_TextChanged(object sender, EventArgs e)
+		private void textBox2_TextChanged(object sender, EventArgs e)
 		{
 
 		}
